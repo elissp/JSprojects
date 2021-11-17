03d3e7908b01f19535586d426fcc013fdb18dacc
Athanasios Tsoustas <60882323+thTs0ustas@users.noreply.github.com>
2021-11-17 21:15:45 +0200

    functional window.open() - added links to social

--- MyProject -Thanos Tsoustas/js/script.js
+++ MyProject -Thanos Tsoustas/js/script.js
@@ -1,8 +1,21 @@
+let linkToHome = document.getElementById("home");
 let linkToAboutUs = document.getElementById("about-us");
-const openRequestedUrl = () => window.open("index_2.html", "_self");
-let main = document.getElementById("main");
+let linkToContact = document.getElementById("contact");
+const openRequestedUrlHome = () => {
+  window.open("index.html", "_self");
+};
+const openRequestedUrlAboutUs = () => {
+  window.open("index_2.html", "_self");
+};
+const openRequestedUrlContact = () => {
+  window.open("index_3.html", "_self");
+};
 
-linkToAboutUs.addEventListener("click", openRequestedUrl);
+linkToHome.addEventListener("click", openRequestedUrlHome);
+linkToAboutUs.addEventListener("click", openRequestedUrlAboutUs);
+linkToContact.addEventListener("click", openRequestedUrlContact);
+
+let main = document.getElementById("main");
 
 let myButton = document.getElementById("myButton");
 
