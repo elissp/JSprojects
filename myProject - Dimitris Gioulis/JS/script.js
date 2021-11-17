87f46b3ec3c1865eaedd7ae037f1ceddf9cfb569
Dimitris Gioulis <78109884+Dimitris-Gs@users.noreply.github.com>
2021-11-17 19:09:43 +0200

    adding Dimitris Gioulis Folder

---
+++ myProject - Dimitris Gioulis/JS/script.js
@@ -0,0 +1,11 @@
+function openIndex () {
+  window.open("index.html", "_blank")
+}
+
+function openAbout () {
+  window.open("about.html", "_blank")
+}
+
+function openContact () {
+  window.open("contact.html", "_blank")
+}
