b81cf432191d5d96eda16b4f4935ff035f3e7922
EnterGithubUsernam3 <93727477+EnterGithubUsernam3@users.noreply.github.com>
2021-11-17 19:31:35 +0200

    Add files via upload

---
+++ myProject-VassilisMourloukos/js/positions.js
@@ -0,0 +1,11 @@
+function openAboutUs(){
+window.open("aboutus.html","abtus",);
+}
+
+function openHomePage(){
+    window.open("index.html","hom3",);
+}
+function openContactUs(){
+    window.open("contactus.html","cntct",)
+}
+
