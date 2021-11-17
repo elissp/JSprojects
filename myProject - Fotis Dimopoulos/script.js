9152ff60837c59820641f79ba90402c9ae926021
collemwest <93729078+collemwest@users.noreply.github.com>
2021-11-17 16:18:01 +0200

    Add files via upload

---
+++ myProject - Fotis Dimopoulos/script.js
@@ -0,0 +1,11 @@
+function openHomepage() {
+    window.open("index.html", "_parent")
+}
+
+function openAboutMe() {
+    window.open("about_me.html", "_parent")
+}
+
+function openContact() {
+    window.open("contact.html", "_parent")
+}
