904d824d0ba82f296651fb4c7935678f63522f31
Konstantina Savvidi <ksavvidi@hotmail.com>
2021-11-18 16:00:08 +0200

    Add files via upload

---
+++ myProject-Konstantina Savvidi/js/script.js
@@ -0,0 +1,9 @@
+function slide1() {
+  window.open("index.html", "_self");
+}
+function slide2() {
+  window.open("contact.html", "_self");
+}
+function slide3() {
+  window.open("aboutus.html", "_self");
+}
