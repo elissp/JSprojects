41edf8beb30f12296d9555244e17959a7394cacc
Evangelap <93728761+Evangelap@users.noreply.github.com>
2021-11-18 14:07:17 +0200

    Add files via upload

---
+++ myProject_Papageorgiou/js/main.js
@@ -0,0 +1,14 @@
+
+function button() {
+  document.documentElement.scrollTop = 0; 
+} 
+
+function tab1(){
+    window.open("index.html","_parent")
+}
+function tab2(){
+    window.open("index2.html","_parent")
+}
+function tab3(){
+    window.open("index3.html","_parent")
+}
