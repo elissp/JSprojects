075013638c159e2fc435c7e601a1c80462a5d872
Dimitris Gioulis <78109884+Dimitris-Gs@users.noreply.github.com>
2021-11-18 12:21:33 +0200

    change target to self fix bg-image add back top btn

--- myProject - Dimitris Gioulis/JS/script.js
+++ myProject - Dimitris Gioulis/JS/script.js
@@ -1,11 +1,15 @@
 function openIndex () {
-  window.open("index.html", "_blank")
+  window.open("index.html", "_self")
 }
 
 function openAbout () {
-  window.open("about.html", "_blank")
+  window.open("about.html", "_self")
 }
 
 function openContact () {
-  window.open("contact.html", "_blank")
+  window.open("contact.html", "_self")
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
 }
