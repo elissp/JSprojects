6b2dc96cf85b99cc2be5a6337f902052cfaa64ed
KonstantinosDiamantogiannis <93727252+KonstantinosDiamantogiannis@users.noreply.github.com>
2021-11-18 17:59:01 +0200

    Add files via upload
    only js and header/footer part is finished.

---
+++ myProject - Diamantogiannis Konstantinos/js/script.js
@@ -0,0 +1,15 @@
+function openHomePage() {
+    window.open("16_11.html", "_parent")
+}
+
+function openAboutMe() {
+    window.open("About_Me.html", "_parent")
+}
+
+function openContact() {
+    window.open("Contact.html", "_parent")
+}
+
+function button() {
+    document.documentElement.scrollTop = 0; 
+  } 
