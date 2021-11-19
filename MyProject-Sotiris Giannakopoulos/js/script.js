0670a29002b147bb8c3077cc1fa4f978639e428e
sotigian <93728766+sotigian@users.noreply.github.com>
2021-11-19 09:06:33 +0200

    Add files via upload

---
+++ MyProject-Sotiris Giannakopoulos/js/script.js
@@ -0,0 +1,17 @@
+function openHome() {
+    window.open("../home.html", "_self");
+}
+
+
+function openAbout_me() {
+    window.open("../about_me.html", "_self");
+}
+
+
+function openContact() {
+    window.open("../contact.html", "_self");
+}
+
+function button() {
+    document.documentElement.scrollTop = 0; 
+  } 
