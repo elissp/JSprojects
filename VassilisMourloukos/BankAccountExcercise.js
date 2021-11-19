20359bcb9b30128c1b62d7de29c031d6c82fa1a9
EnterGithubUsernam3 <93727477+EnterGithubUsernam3@users.noreply.github.com>
2021-11-19 21:46:04 +0200

    Add files via upload

---
+++ VassilisMourloukos/BankAccountExcercise.js
@@ -0,0 +1,66 @@
+//Object constructor method
+
+function BankAcount(OwnerName,Balance,Withdraw,Deposit){
+    this.OwnerName = OwnerName;
+    this.Balance = Balance;
+    this.Withdraw =function() {
+        var userInput =parseInt(prompt("Please enter the requested ammount "));
+            while(isNaN(userInput)){
+                console.log ("Enter a number ");
+            }
+            if(userInput>this.Balance){
+                alert("Not enought funds ");
+               
+            }
+            else{
+                this.Balance = this.Balance - userInput;
+            }
+            console.log(this.Balance);
+    };
+    this.Deposit = function() {
+        var userInput =parseInt(prompt("Please enter the requested ammount "));
+        while(isNaN(userInput)){
+            console.log ("Enter a number ");
+        }
+        this.Balance = this.Balance + userInput ;
+    }
+    console.log(this.Balance);
+}
+
+
+const VaggelisMarinakisPsila = new BankAcount("Vaggelis Marinakis",1000000,BankAcount.Withdraw,BankAcount.Deposit);
+
+VaggelisMarinakisPsila.Withdraw();
+VaggelisMarinakisPsila.Deposit();
+
+//Direct Method
+var anotherBankAccount = {
+    Name:"Dimitris Gianakopoulos",
+    Balance : 100,
+    Withdraw : function() {
+        var userInput =parseInt(prompt("Please enter the requested ammount "));
+            while(isNaN(userInput)){
+                console.log ("Enter a number ");
+            }
+            if(userInput>this.Balance){
+                alert("Not enought funds ");
+               
+            }
+            else{
+                this.Balance = this.Balance - userInput;
+            }
+            console.log(this.Balance);
+} ,
+    Deposit: function() {
+        var userInput =parseInt(prompt("Please enter the requested ammount "));
+        while(isNaN(userInput)){
+            console.log ("Enter a number ");
+        }
+        this.Balance = this.Balance + userInput ;
+        console.log(this.Balance);
+    }
+    
+}
+anotherBankAccount.Deposit();
+
+
