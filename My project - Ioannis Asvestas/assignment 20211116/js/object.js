96f05679218d58bcfe4cbaefe2176c5cfb057e8e
jeanAsvestas <93285517+jeanAsvestas@users.noreply.github.com>
2021-11-19 13:29:51 +0200

    Add files via upload

---
+++ My project - Ioannis Asvestas/assignment 20211116/js/object.js
@@ -0,0 +1,51 @@
+//functions to pages.
+
+
+function homePage() {
+var homing= window.open("index.html", "_self");
+}
+function contactMe() {
+    var myWindow = window.open("contact.html", "_self");
+}
+function aboutUs() {
+    var myWindow = window.open("aboutus.html", "_self");
+}
+function instagramPage() {
+    var myWindow = window.open("https://www.instagram.com/", "_blank");
+}
+function facebookPage() {
+    var myWindow = window.open("https://www.facebook.com/", "_blank");
+}
+function twitterPage() {
+    var myWindow = window.open("https://www.twitter.com/", "_blank");
+    console.log(new Date().getDay());
+}
+
+
+//topPage button
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+  }
+
+
+  
+function topPage() {
+    window.scrollTo({        
+        left:0,
+        top:0,
+        behavior: "smooth"       
+    });     
+}
+
+//search button function
+function searchButton() {
+    alert("nice try! Cant search anything...");
+}
+
