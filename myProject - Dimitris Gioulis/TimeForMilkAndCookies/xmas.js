05e55ec4cd6664703b015ebc185f7a927c85c097
Dimitris Gioulis <78109884+Dimitris-Gs@users.noreply.github.com>
2021-11-25 17:28:09 +0200

    cahnges in timeformilkandcookies

--- myProject - Dimitris Gioulis/TimeForMilkAndCookies/xmas.js
+++ myProject - Dimitris Gioulis/TimeForMilkAndCookies/xmas.js
@@ -1,13 +1,15 @@
-const d = new Date('December 24,');
-
 function timeForMilkAndCookies() {
-  let day = d.getDay();
-  let month = m.getMonth()+1;
-  if (day&&month == d ) {
-    console.log("Time for milk and cookies");
+  var newDate = document.getElementById("ChristmasEve").value
+  newDate = new Date(newDate);
+
+  var day = newDate.getDate();
+  var month = newDate.getMonth()+1;
+
+  if (day === 24 && month === 12) {
+    alert("Time for milk and cookies!");
     return true;
   } else {
-    console.log("Not time for milk and cookies");
+    alert("Not time for milk and cookies");
     return false;
   }
 }
