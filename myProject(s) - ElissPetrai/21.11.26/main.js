bcbb5f0f04b1d0b396fb77d84e18f2162a2f7246
new <15091642+ceidas@users.noreply.github.com>
2021-11-26 18:59:11 +0000

    new file

---
+++ myProject(s) - ElissPetrai/21.11.26/main.js
@@ -0,0 +1,31 @@
+//function myFunction () {
+document.getElementById('myFunc').addEventListener('click', () => { 
+    //access child node
+    const child = document.body.children[0].children[1];
+    console.log(child)
+    //change color
+    const firstElem = document.body.children[0].children[1].style.color = 'red';
+    const fourthElem = document.body.children[0].children[3].style.color = 'red';
+
+    //change font
+    const newFont = document.body.children[0].children[0].style.fontFamily = 'Helvetica';
+
+
+    // create New Li
+    const newListItem = document.createElement('li');
+
+    //assign the text value to LI
+    newListItem.textContent = 'FFF' ;
+    
+    //Add in list
+
+    document.body.children[0].insertBefore(newListItem , document.body.children[0].children[4])
+
+    //bold font
+    const boldNewElement = document.body.children[0].children[4].style.fontWeight = '900'
+
+    //Invisible 
+    const invElement = document.body.children[0].children[4].style.visibility = 'hidden'
+
+     })
+//}
