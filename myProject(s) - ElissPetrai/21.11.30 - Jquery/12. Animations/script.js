81eea2c7c8701989212c0ae14a7a64b28d224fbc
new <15091642+ceidas@users.noreply.github.com>
2021-11-30 19:17:07 +0000

    jquery

--- myProject(s) - ElissPetrai/21.11.30 - Jquery/12. Animations/script.js
+++ myProject(s) - ElissPetrai/21.11.30 - Jquery/12. Animations/script.js
@@ -5,5 +5,22 @@ $(document).ready(function(){
 
     $("#button2").click(function(){
         $("p").fadeIn();
+    });
+
+    $("#button3").click(function(){
+        $("img").animate(
+        {
+            opacity: '0.2',
+            left:'250px'
+        });
+
+    });
+
+    $("#button4").click(function(){
+        $('img').hide("slow");
+    })
+
+    $("#button5").click(function(){
+        $('img').show("fast");
     })
 });
